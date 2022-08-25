78ce4918df462201f5a3e1f207cb4bfc009ee2f3
simonee <simone.schimmel@gmx.net>
2022-08-25 10:20:58 +0200

    sensMat adapt shape to matlab version

--- pythonImplentation/subfunctions/sensitivityMatrix.py
+++ pythonImplentation/subfunctions/sensitivityMatrix.py
@@ -1,14 +1,17 @@
 #Flächenströme der Dreieck um jeden Knoten aufintegrieren
+from itertools import zip_longest
 import numpy as np
 
-def getSensitivityMatrix(mesh,target,n):
+def getSensitivityMatrix(mesh,target,n):#!!!! dimension passt nicht ...
     biotSavatCoeff = 10**(-7)
     sensitivityMatrix = []
     [u,v,gaussWeight] = gaussLegendreIntegrationPointsTriangle(n)
     trianglesPerNode = [len(mesh.neighbours[x]) for x in range(len(mesh.neighbours))]
-    xTarget,yTarget,zTarget = target.vertices
+    xTarget,yTarget,zTarget = target.vertices[:,0],target.vertices[:,1],target.vertices[:,2]
+    #print(np.shape(mesh.vertices), np.shape(target.vertices))
+    xAll,yAll,zAll=[],[],[]
     for nodeIndex in range(len(mesh.vertices)):
-        dCx,dCy,dCz =[0,0,0],[0,0,0],[0,0,0]
+        dCx,dCy,dCz = np.zeros(len(xTarget)),np.zeros(len(xTarget)),np.zeros(len(xTarget))
         for triangleIndex in range(trianglesPerNode[nodeIndex]):
             nodePoint,pointB,pointC = mesh.faces[mesh.neighbours[nodeIndex][triangleIndex]]
             nodeX,nodeY,nodeZ = mesh.vertices[nodePoint]
@@ -17,10 +20,10 @@ def getSensitivityMatrix(mesh,target,n):
             vX,vY,vZ = mesh.current[mesh.neighbours[nodeIndex][triangleIndex]]
 
             for gaussIndex in range(len(gaussWeight)):
-                xGaussInUV = nodeX*u[gaussIndex]+bX*v[gaussIndex]+cX*(1-u[gaussIndex]-v[gaussIndex])
-                yGaussInUV = nodeY*u[gaussIndex]+bY*v[gaussIndex]+cY*(1-u[gaussIndex]-v[gaussIndex])
-                zGaussInUV = nodeZ*u[gaussIndex]+bZ*v[gaussIndex]+cZ*(1-u[gaussIndex]-v[gaussIndex])
-                distanceNorm = (np.square(xGaussInUV-xTarget)+np.square(yGaussInUV-yTarget)+np.square(zGaussInUV-zTarget))**(-3/2)#for biot savat
+                xGaussInUV = nodeX*u[gaussIndex]+bX*v[gaussIndex]+cX*(1-u[gaussIndex]-v[gaussIndex])#scalar
+                yGaussInUV = nodeY*u[gaussIndex]+bY*v[gaussIndex]+cY*(1-u[gaussIndex]-v[gaussIndex])#scalar
+                zGaussInUV = nodeZ*u[gaussIndex]+bZ*v[gaussIndex]+cZ*(1-u[gaussIndex]-v[gaussIndex])#scalar
+                distanceNorm = (np.square(xGaussInUV-xTarget)+np.square(yGaussInUV-yTarget)+np.square(zGaussInUV-zTarget))**(-3/2)#for biot savat #len of target
                 dCx = dCx + ((-1)*vZ*(yTarget-yGaussInUV)+ vY*(zTarget-zGaussInUV))*distanceNorm *2 *mesh.areas[mesh.neighbours[nodeIndex][triangleIndex]]* gaussWeight[gaussIndex]
                 dCy = dCy + ((-1)*vX*(zTarget-zGaussInUV)+ vZ*(xTarget-xGaussInUV))*distanceNorm *2 *mesh.areas[mesh.neighbours[nodeIndex][triangleIndex]]* gaussWeight[gaussIndex]
                 dCz = dCz + ((-1)*vY*(xTarget-xGaussInUV)+ vX*(yTarget-yGaussInUV))*distanceNorm *2 *mesh.areas[mesh.neighbours[nodeIndex][triangleIndex]]* gaussWeight[gaussIndex]
@@ -28,8 +31,14 @@ def getSensitivityMatrix(mesh,target,n):
             dCx *= biotSavatCoeff
             dCy *= biotSavatCoeff
             dCz *= biotSavatCoeff
-        sensitivityMatrix.append([dCx,dCy,dCz])
-        #print(sensitivityMatrix) #achtung viel output!
+        xAll.append(dCx)
+        yAll.append(dCy)
+        zAll.append(dCz)
+    xAll = np.transpose(xAll)
+    yAll = np.transpose(yAll)
+    zAll = np.transpose(zAll)
+    sensitivityMatrix=[xAll,yAll,zAll]    
+    #print(np.shape(sensitivityMatrix))
     return sensitivityMatrix
 
 def gaussLegendreIntegrationPointsTriangle(n):
