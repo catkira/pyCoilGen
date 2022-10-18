29f5e2fb9dbdf0891a473c2f4d88e3b15810ef9e
simonee <simone.schimmel@gmx.net>
2022-10-18 13:10:06 +0200

    resistanceMatrix corrected

--- subfunctions/resistanceMatrix.py
+++ subfunctions/resistanceMatrix.py
@@ -3,46 +3,69 @@ from .defineTargetField import distanceBetweenPoints
 
 def getResistanceMatrix(mesh,materialFactor):
     '''returns the resistance Matrix for the given mesh'''
-    
-    # get neighbours nodes 
-    neighbourPairs = np.where(getNeighbourhoodMatrix(mesh))
-
-    # define matrix elements that should get a value (neighbours + diagonal elements)
-    meshEdgesNonUnique = [np.concatenate([np.linspace(0,263,264),neighbourPairs[1]]) ,np.concatenate([np.linspace(0,263,264),neighbourPairs[0]])]
-
+    nodeAdjacencyMatrix=getNeighbourhoodMatrix(mesh)
+    #nodeAdjacencyMatrix = nodeAdjacencyMatrix | np.matrix.getH(nodeAdjacencyMatrix)
+    neighbourPairs = np.array(np.where(nodeAdjacencyMatrix))
+    neighbourPairs = neighbourPairs[:,neighbourPairs[1].argsort()]
+   
+    matElementsShouldGetValue = [np.concatenate([np.linspace(0,263,264),neighbourPairs[0]]) ,np.concatenate([np.linspace(0,263,264),neighbourPairs[1]])] 
+    #matElementsShouldGetValue = neighbourPairs
+    with open('test2.txt', 'w') as f:
+        for i in range(len(matElementsShouldGetValue[0])):
+            f.write("[0]" + str(matElementsShouldGetValue[0][i]) + "[1]" + str(matElementsShouldGetValue[1][i]))
+            f.write('\n')
     # #useless at this position ...
     # nodeAdjacencyMatrix = nodeAdjacencyMatrix | np.matrix.getH(nodeAdjacencyMatrix)
     
     # #calc matrix of spatial distances - at the Moment not needed anywhere ...
     # nodalNeighbourMatrix = getSpatialDistancesMatrix(mesh)
     
-    #calc resistance matrix
     resistanceMatrix = np.zeros((len(mesh.vertices),len(mesh.vertices)),dtype=float)
-    #look at each "want to change" matrix positions
-    for edgeInd in range(len(meshEdgesNonUnique[0])):
-        nodeInd1 = int(meshEdgesNonUnique[0][edgeInd])
-        nodeInd2 = int(meshEdgesNonUnique[1][edgeInd])
-        #find the triangles that surround both nodes
-        overlappingTriangles = intersect_mtlb(mesh.neighbours[nodeInd1],mesh.neighbours[nodeInd2])
-        if overlappingTriangles:
-            resistanceSum =0
-            for overlappTriangle in overlappingTriangles:
-                firstNodeTrianglePosition = compareMultipleElementsBoolean(mesh.neighbours[nodeInd1],overlappTriangle)
-                secondNodeTrianglePosition = compareMultipleElementsBoolean(mesh.neighbours[nodeInd2],overlappTriangle)
-                triangelArea = mesh.areas[overlappTriangle]
-
-                primaryCurrent,secondaryCurrent = [0,0,0],[0,0,0]
-                for i in range(len(mesh.neighbourcurrents[nodeInd1])):#hier werden die falschen dinge kombiniert
-                    primaryCurrent += mesh.neighbourcurrents[nodeInd1][i]*firstNodeTrianglePosition[i]
-                for i in range(len(mesh.neighbourcurrents[nodeInd2])):#hier werden die falschen dinge kombiniert
-                    secondaryCurrent += mesh.neighbourcurrents[nodeInd2][i]*secondNodeTrianglePosition[i]
-                resistanceSum += np.dot(primaryCurrent,secondaryCurrent)*triangelArea**2
-            resistanceMatrix[nodeInd1][nodeInd2] = resistanceSum
-    resistanceMatrix = 2*resistanceMatrix*materialFactor
+    
+    for elementIndex in range(len(matElementsShouldGetValue[0])):
+        nodeInd1 = int(matElementsShouldGetValue[0][elementIndex])
+        nodeInd2 = int(matElementsShouldGetValue[1][elementIndex])
+        if nodeInd1 == nodeInd2:
+            resistanceSum = 0
+            for triangle in mesh.oneRingList[nodeInd1]:
+                triangelArea = np.linalg.norm(np.cross((mesh.vertices[triangle[0]]-mesh.vertices[nodeInd1]),(mesh.vertices[triangle[1]]-mesh.vertices[nodeInd1])))/2
+                current = (mesh.vertices[triangle[1]]-mesh.vertices[triangle[0]])/(np.linalg.norm(np.cross((mesh.vertices[triangle[0]]-mesh.vertices[nodeInd1]),(mesh.vertices[triangle[1]]-mesh.vertices[nodeInd1]))))
+                resistanceSum = resistanceSum + np.dot(current,current)*triangelArea**2
+            resistanceMatrix[nodeInd1][nodeInd1] = resistanceSum
+        else:
+            trianglesWithBothNodes = [elementInArray(mesh.oneRingList[nodeInd1],nodeInd2),elementInArray(mesh.oneRingList[nodeInd2],nodeInd1)]
+            if trianglesWithBothNodes[0]:
+                resistanceSum =0
+                for triangle in trianglesWithBothNodes[0]:
+                    triangelArea = np.linalg.norm(np.cross((mesh.vertices[triangle[0]]-mesh.vertices[nodeInd1]),(mesh.vertices[triangle[1]]-mesh.vertices[nodeInd1])))/2
+                    primaryCurrent = (mesh.vertices[triangle[1]]-mesh.vertices[triangle[0]])/(np.linalg.norm(np.cross((mesh.vertices[triangle[0]]-mesh.vertices[nodeInd1]),(mesh.vertices[triangle[1]]-mesh.vertices[nodeInd1]))))
+                    partnerElement = getPartnerElement(triangle,trianglesWithBothNodes,nodeInd2)
+                    secondaryCurrent = (mesh.vertices[partnerElement[1]]-mesh.vertices[partnerElement[0]])/(np.linalg.norm(np.cross((mesh.vertices[partnerElement[0]]-mesh.vertices[nodeInd2]),(mesh.vertices[partnerElement[1]]-mesh.vertices[nodeInd2]))))
+                    resistanceSum = resistanceSum + np.dot(primaryCurrent,secondaryCurrent)*(triangelArea**2)
+                resistanceMatrix[nodeInd1][nodeInd2] = resistanceSum
+    resistanceMatrix = resistanceMatrix + np.transpose(resistanceMatrix)
+    resistanceMatrix = resistanceMatrix*materialFactor
     return resistanceMatrix
 
+def getPartnerElement(triangle,trianglesWithBothNodes,nodeInd2):
+    partnerElement =0
+    for i in triangle: 
+        if i != nodeInd2:
+            differentElement = i
+    for j in trianglesWithBothNodes[1]:
+        if differentElement in j:
+            partnerElement = j
+    return partnerElement
 
-def intersect_mtlb(a, b):
+def elementInArray(array,value):
+    '''returns a list with the elements the contain "value"'''
+    solution =[]
+    for element in array:
+        if value in element:
+            solution.append(element)
+    return solution
+
+def elementInAandB(a, b):
     '''returns a list with elements that are in a and b'''
     solution =[]
     for i in a:
